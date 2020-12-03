e9527c72686a79a668b0adf9fc8efe395e99627d
Taimaru Provensal <taimaru@gmail.com>
2020-12-02 18:20:32 -0800

    think i got the good cordinates from trial and error

--- Crop.py
+++ Crop.py
@@ -13,10 +13,21 @@ out_file = open('edit-' + str(sys.argv[1]), 'wb')
 pdf_writer = PyPDF2.PdfFileWriter()
 
 for pagenum in range(pdf_reader.numPages):
+    
+    # get page from in file
     page = pdf_reader.getPage(pagenum)
 
+    # print(page.mediaBox.getUpperLeft())
+    # set top right corner
+    page.cropBox.upperRight = (545, 753)
+    
+    # set lower left corner
+    page.cropBox.lowerLeft = (67, 435)
+    
+    # rotate page
     page.rotateClockwise(90)
 
+    # write to new pdf
     pdf_writer.addPage(page)
 
 pdf_writer.write(out_file)
